28c93931b076833a7999d244e156300cb39b0203
edErikson <38958077+edErikson@users.noreply.github.com>
2025-11-11 12:06:57 +0200

    Add files via upload
    init

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'forge-v7';
+const FILES = ['forge.html', 'manifest.json', 'images/icon-192.png', 'images/icon-512.png'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => response || fetch(e.request))
+    );
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+        })
+    );
+});
